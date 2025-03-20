392d4a10a180aa5d141dec8149f93f8d306333eb
Gabriel Couto de Freitas <gabriel.freitas@techers.local>
2025-03-20 11:22:30 -0300

    paginas de login e cadastro feitas

---
+++ scripts/cadastro.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoInteresse = document.querySelector("#cadastrou");
+
+    botaoInteresse.addEventListener("click", function () {
+        window.location.href = "inicial_interna.html";
+    });
+});
---
+++ scripts/login.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoInteresse = document.querySelector("#cadastrar");
+
+    botaoInteresse.addEventListener("click", function () {
+        window.location.href = "cadastro.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoInteresse = document.querySelector("#forgot-password");
+
+    botaoInteresse.addEventListener("click", function () {
+        alert("Em implementação.")
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoInteresse = document.querySelector("#logou");
+
+    botaoInteresse.addEventListener("click", function () {
+        window.location.href = "inicial_interna.html";
+    });
+});
