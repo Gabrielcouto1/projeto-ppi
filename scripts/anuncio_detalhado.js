efd0de868e8eb87cd4c4c096a9da2f9b2867d9a9
Gabriel Couto de Freitas <gabriel.freitas@techers.local>
2025-03-20 11:02:29 -0300

    mudanças de formatação e padronização

---
+++ scripts/anuncio_detalhado.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(".slide");
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
+
+    function mostrarSlides(n) {
+        if (n >= slides.length) {
+            slideIndex = 0;
+        } else if (n < 0) {
+            slideIndex = slides.length - 1;
+        }
+
+        slides.forEach((slide, index) => {
+            slide.style.display = index === slideIndex ? "block" : "none";
+        });
+    }
+
+    function mudarSlide(n) {
+        slideIndex += n;
+        mostrarSlides(slideIndex);
+    }
+
+    prevButton.addEventListener("click", () => mudarSlide(-1));
+    nextButton.addEventListener("click", () => mudarSlide(1));
+
+    mostrarSlides(slideIndex);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoInteresse = document.querySelector(".ver");
+
+    botaoInteresse.addEventListener("click", function () {
+        window.location.href = "lista_int.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoInteresse = document.querySelector(".excluir");
+
+    botaoInteresse.addEventListener("click", function () {
+        alert("Em implementação.")
+    });
+});
